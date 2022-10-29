4ad8875f1056ed5972b43c369993a18177f383b1
ChineseMuffin <61895374+ChineseMuffin@users.noreply.github.com>
2022-10-29 11:40:51 +0000

    日付指定でその前後を取得できるように改良

--- src/mannaka_birthday.py
+++ src/mannaka_birthday.py
@@ -1,47 +1,96 @@
 from datetime import date, timedelta
+from dataclasses import dataclass
 
 
-"""
-誕生日が閏日は未対応
-"""
+@dataclass
+class Birthday:
+    month: int
+    day: int
+
+    def is_intercalary(self):
+        return self.day == 29 and self.month == 2
 
 
 class MannakaBirthday:
-    INTERCALARY_CALIB = timedelta(1)
+    MAX_ITERATION = 4000
 
-    def __init__(self, birthday1: date, birthday2: date):
+    def __init__(
+        self, birthday1: Birthday, birthday2: Birthday, year1: int, year2: int
+    ):
+        assert year1 <= year2
 
-        assert 0 <= (birthday2 - birthday1).days <= 366
-
-        self._intercalary = False
+        self._year1 = year1
+        self._year2 = year2
 
         self._birthday1 = birthday1
         self._birthday2 = birthday2
 
-        if (birthday1.day == 29 and birthday1.month == 2) or (
-            birthday2.day == 29 and birthday2.month == 2
-        ):
-            self._intercalary = True
-            self._birthday1 = birthday1 + MannakaBirthday.INTERCALARY_CALIB
-            self._birthday2 = birthday2 + MannakaBirthday.INTERCALARY_CALIB
+    def next(self, target: date | None = None) -> "MannakaBirthday":
+        if target is None:
+            return self._next_base()
 
-    def next(self) -> "MannakaBirthday":
-        birthday1_year = self._birthday1.year
-        next_birthday1 = self._birthday1.replace(birthday1_year + 1)
-        birthday2 = self._birthday2
-        return MannakaBirthday(birthday2, next_birthday1)
+        m = self
 
-    def previous(self) -> "MannakaBirthday":
-        birthday2_year = self._birthday2.year
-        previous_birthday2 = self._birthday2.replace(birthday2_year - 1)
-        birthday1 = self._birthday1
-        return MannakaBirthday(previous_birthday2, birthday1)
+        delta_days = (m.mannaka_date() - target).days
+
+        if delta_days == 0:
+            return m
+
+        if delta_days > 0:
+            for _ in range(MannakaBirthday.MAX_ITERATION):
+                is_past = m.mannaka_date() < target
+                if is_past:
+                    return m._next_base()
+                m = m.previous()
+
+        if delta_days < 0:
+            for _ in range(MannakaBirthday.MAX_ITERATION):
+                is_future = m.mannaka_date() > target
+                if is_future:
+                    return m
+                m = m._next_base()
+
+        raise Exception()
+
+    def previous(self, target: date | None = None) -> "MannakaBirthday":
+        if target is None:
+            return self._previous_base()
+
+        return self.next(target)._previous_base()
 
     def mannaka_date(self) -> date:
-        date1 = self._birthday1
-        date2 = self._birthday2
+        has_intercalary = (
+            self._birthday1.is_intercalary() or self._birthday2.is_intercalary()
+        )
+
+        if has_intercalary:
+            if self._birthday1.is_intercalary():
+                date1 = date(self._year1, 3, 1)
+            else:
+                date1 = date(
+                    self._year1, self._birthday1.month, self._birthday1.day + 1
+                )
+            if self._birthday2.is_intercalary():
+                date2 = date(self._year2, 3, 1)
+            else:
+                date2 = date(
+                    self._year2, self._birthday2.month, self._birthday2.day + 1
+                )
+        else:
+            date1 = date(self._year1, self._birthday1.month, self._birthday1.day)
+            date2 = date(self._year2, self._birthday2.month, self._birthday2.day)
+
         delta = date2 - date1
-        mannaka = date1 + timedelta(delta.days / 2)
-        if self._intercalary:
-            mannaka -= MannakaBirthday.INTERCALARY_CALIB
+        mannaka = date1 + timedelta(delta.days / 2) - timedelta(has_intercalary)
+
         return mannaka
+
+    def _next_base(self) -> "MannakaBirthday":
+        return MannakaBirthday(
+            self._birthday2, self._birthday1, self._year2, self._year1 + 1
+        )
+
+    def _previous_base(self) -> "MannakaBirthday":
+        return MannakaBirthday(
+            self._birthday2, self._birthday1, self._year2 - 1, self._year1
+        )
--- src/test_mannaka_birthday.py
+++ src/test_mannaka_birthday.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import date, timedelta
 
-from mannaka_birthday import MannakaBirthday
+from mannaka_birthday import Birthday, MannakaBirthday
 
 
 class Test(unittest.TestCase):
@@ -9,36 +9,49 @@ class Test(unittest.TestCase):
         return super().setUp()
 
     def test_1(self):
-        birthday1 = date(2000, 2, 28)
-        birthday2 = date(2000, 3, 1)
+        birthday1 = Birthday(3, 1)
+        birthday2 = Birthday(2, 28)
 
-        m = MannakaBirthday(birthday1, birthday2)
+        m = MannakaBirthday(birthday1, birthday2, 2000, 2000)
 
         for i in range(10):
             mannaka = m.mannaka_date()
             m = m.next()
-            print(mannaka)
+            print(i, mannaka)
 
         for i in range(5):
             mannaka = m.mannaka_date()
             m = m.previous()
-            print(mannaka)
+            print(i, mannaka)
 
     def test_intercalary(self):
-        birthday1 = date(2000, 2, 29)
-        birthday2 = date(2000, 2, 29)
+        birthday1 = Birthday(2, 29)
+        birthday2 = Birthday(2, 29)
 
-        m = MannakaBirthday(birthday1, birthday2)
+        m = MannakaBirthday(birthday1, birthday2, 2000, 2000)
 
         for i in range(10):
             mannaka = m.mannaka_date()
             m = m.next()
-            print(mannaka)
+            print(i, mannaka)
 
         for i in range(5):
             mannaka = m.mannaka_date()
             m = m.previous()
-            print(mannaka)
+            print(i, mannaka)
+
+    def test_next_previous(self):
+        birthday1 = Birthday(3, 1)
+        birthday2 = Birthday(2, 28)
+        today = date(2022, 10, 29)
+        m = MannakaBirthday(birthday1, birthday2, 2000, 2000)
+
+        md = m.mannaka_date()
+        print(md)
+        md = m.next(today).mannaka_date()
+        print(md)
+        md = m.previous(today).mannaka_date()
+        print(md)
 
 
 if __name__ == "__main__":
